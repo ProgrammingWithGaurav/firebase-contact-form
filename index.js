d89f03417af48f60d2b1aa13384f9053054a0640
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-15 21:59:31 +0530

    adding success alert on form submit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let username = document.getElementById('name');
 let email = document.getElementById('email');
 let message = document.getElementById('message');
 let alertMessage = document.getElementById('alert');
+let successMessage = document.getElementById('success');
 
 function send() {
     if (username.value !== '' && email.value !== '' && message.value !== '') {
@@ -10,6 +11,10 @@ function send() {
             email: email.value,
             message: message.value,
         })
+        successMessage.classList.remove('hidden');
+        setTimeout(() => {
+          successMessage.classList.add('hidden');
+        }, 3000)
         username.value = ''
         email.value = ''
         message.value = ''
