5efd2e890dafc38e5219ba6cdf348355eea57e65
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-15 20:44:51 +0530

    Adding validation and alerts

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let username = document.getElementById('name');
 let email = document.getElementById('email');
-let messsage = document.getElementById('messsage');
+let message = document.getElementById('message');
+let alertMessage = document.getElementById('alert');
 
-function send(event) {
-    let message = document.getElementById('message')
-    db.collection("contact-list").add({
-        name : username.value,
-        email: email.value,
-        message: message.value,
-    })
-    username.value = ''
-    email.value = ''
-    message.value = ''
+function send() {
+    if (username.value !== '' && email.value !== '' && message.value !== '') {
+        db.collection("contact-list").add({
+            name: username.value,
+            email: email.value,
+            message: message.value,
+        })
+        username.value = ''
+        email.value = ''
+        message.value = ''
+    }
+    else {
+        alertMessage.classList.remove('hidden');
+        setTimeout(()=>{
+            alertMessage.classList.add('hidden');
+        },3000)
+    }
 }
